740e2d6cc34ab8f36de49d5b7872b16c6b89b62f
Anil Altan <anlaltnn@gmail.com>
2023-12-07 08:41:13 +0300

    Adding tests

--- hafta4-odev2/emoji-search-master/src/EmojiCopy.test.js
+++ hafta4-odev2/emoji-search-master/src/EmojiCopy.test.js
@@ -2,14 +2,20 @@ import React from "react";
 import App from "./App";
 import "@testing-library/jest-dom";
 import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
 
 test("Clicking on an emoji copies the emoji", () => {
   // Rendering the App component
   render(<App />);
   // Retrieving all elements on the screen with the "emoji-result-row" data-testid
   const clicks = screen.getAllByTestId("emoji-result-row");
+  // Retrieving the filter input element by its placeholder text
+  const filterInput = screen.getByPlaceholderText("Filter emojis...");
 
-  // Asserting that the first element has the "data-clipboard-text" attribute
-  // This assumes that the data-clipboard-text attribute is present for the first element
-  expect(clicks[0]).toHaveAttribute("data-clipboard-text");
+  // Simulating a click on the first emoji (assuming there is at least one emoji)
+  userEvent.click(clicks);
+  // Simulating a paste event on the filter input using the clicked emoji
+  userEvent.paste(filterInput, clicks);
+  // Asserting that the filter input value has been updated
+  expect(filterInput === 1);
 });
